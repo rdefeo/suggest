37271355eaca9bc86c03adc452b031a948bee2ce
Rob De Feo <robertodefeo@hotmail.com>
2015-03-27 13:26:10 +0100

    application_id closes #17

--- suggest/data/suggestion.py
+++ suggest/data/suggestion.py
@@ -1,6 +1,6 @@
 import logging
 from suggest.data.data import Data
-
+from datetime import datetime
 __author__ = 'robdefeo'
 
 
@@ -8,14 +8,19 @@ class Suggestion(Data):
     LOGGER = logging.getLogger(__name__)
     collection_name = "suggestions"
 
-    def insert(self, items, locale, context_id, session_id, page, page_size):
+    def insert(self, items, locale, application_id, context_id, session_id, page, page_size, now=None):
+        if now is None:
+            now = datetime.now()
+
         self.collection.insert(
             {
                 "items": items,
                 "locale": locale,
+                "application_id": application_id,
                 "context_id": context_id,
                 "session_id": session_id,
                 "page": page,
-                "page_size": page_size
+                "page_size": page_size,
+                "created": now.isoformat()
             }
         )
--- suggest/handlers/root.py
+++ suggest/handlers/root.py
@@ -18,12 +18,33 @@ class Root(RequestHandler):
     def get(self, *args, **kwargs):
         self.set_header('Content-Type', 'application/json')
         session_id = self.get_argument("session_id", None)
+        application_id = self.get_argument("application_id", None)
         locale = self.get_argument("locale", None)
         raw_page = self.get_argument("page", None)
         raw_page_size = self.get_argument("page_size", None)
         raw_context = self.get_argument("context", None)
 
-        if raw_page is None:
+        if application_id is None:
+            self.set_status(412)
+            self.finish(
+                json_encode(
+                    {
+                        "status": "error",
+                        "message": "missing param=application_id"
+                    }
+                )
+            )
+        elif session_id is None:
+            self.set_status(412)
+            self.finish(
+                json_encode(
+                    {
+                        "status": "error",
+                        "message": "missing param=session_id"
+                    }
+                )
+            )
+        elif raw_page is None:
             self.set_status(412)
             self.finish(
                 {
@@ -75,6 +96,7 @@ class Root(RequestHandler):
                     self.suggestor.get_reasons(context, suggestion_response["suggestions"],  minimum, maximum),
                     locale,
                     ObjectId(context["_id"]),
+                    ObjectId(application_id),
                     ObjectId(session_id),
                     page,
                     page_size
