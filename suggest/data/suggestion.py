db02aa676fe0fff99d3f19ee0568e545a364eaf9
Rob De Feo <robertodefeo@hotmail.com>
2015-04-28 11:47:45 +0200

    moving things around still

--- suggest/data/suggestion.py
+++ suggest/data/suggestion.py
@@ -3,6 +3,10 @@ from bson import ObjectId
 from suggest.data.data import Data
 from datetime import datetime
 from suggest import __version__
+from bson.code import Code
+from bson.son import SON
+from datetime import datetime, timedelta
+
 __author__ = 'robdefeo'
 
 
@@ -39,3 +43,64 @@ class Suggestion(Data):
             data["user_id"] = user_id
 
         self.collection.insert(data)
+
+    def map_product_result_listing(self, now=datetime.now(), days_behind=30):
+        """
+        Used to get the more displayed results
+        :param _id_type_list: which types to consider in detection
+        :return:
+        """
+
+        mapper = Code("""
+            function(){
+                for(var i in this.items) {
+                    var item = this.items[i]
+                    emit(
+                        {
+                            product_id: item._id
+                        },
+                        {
+                            listing_count: 1
+                        }
+                    )
+                }
+            }
+        """)
+        reducer = Code("""
+            function(key, values) {
+                var total = 0;
+                values.forEach(function(value) {
+                    total += value.listing_count;
+                });
+
+                return {
+                    listing_count: total
+                };
+
+            }
+        """)
+
+        timestamp = (now - timedelta(days=days_behind)).isoformat()
+
+        self.LOGGER.info("generate=product_result_listing,timestamp=%s,out=product_result_listing,action=replace,db=generate", timestamp)
+
+        result = self.collection.map_reduce(
+            mapper,
+            reducer,
+            query={
+                "type": "results",
+                "timestamp": {
+                    "$gte": timestamp
+                }
+            },
+            out=SON(
+                [
+                    ("replace", "product_result_listing"),
+                    ("db", "generate")
+                ]
+            )
+
+        )
+
+        return result
+
